8052b6f97c5073782bd9b64b71cbcfbc9bf9aa22
Niko Lazaris <niko@meetup.com>
2016-10-21 12:14:33 -0400

    add cron job/ basic slack commands

--- bot.es6.js
+++ bot.es6.js
@@ -2,6 +2,52 @@
  * Ride Along Bot
  */
 
+import { TOKEN } from './.config';
+import { CronJob } from 'cron';
 import { RtmClient, WebClient } from '@slack/client';
+import { RTM_EVENTS, CLIENT_EVENTS } from '@slack/client';
 
-console.log('Here');
+
+const rtm = new RtmClient(TOKEN, {logLevel: 'info'});
+
+const usage = ":robot_face:*RIDE ALONG * - Your dedicated pairing slackbot";
+
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
+	console.log('Connection opened');
+});
+
+rtm.on(CLIENT_EVENTS.RTM.DISCONNECT, () => {
+	console.log('Connection closed');
+});
+
+rtm.on(RTM_EVENTS.MESSAGE, (data) => {
+	console.log(data);
+	let command = data.text;
+	const botId = `<@${rtm.activeUserId}>`;
+
+	if( command.substring(0, botId.length) === botId) {
+		command = command.substring(botId.length + 1).trim();
+	}
+
+	switch (command) {
+		case 'help':
+			rtm.sendMessage(usage, data.channel);
+			break;
+	}
+
+});
+
+const pairUsers = () => {
+	// TODO Grab users from channel
+	// create chats b/t them
+};
+
+const job = new CronJob({
+	cronTime: '00 30 10 * * 1',
+	onTick: pairUsers,
+	start: false,
+	timeZone: 'America/New_York'
+});
+
+
+rtm.start();
